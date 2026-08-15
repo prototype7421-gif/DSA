99d1bca71a857d84d5bcdc9a8c68a4c3fef6f5d7
prototype7421-gif <prototype7421@gmail.com>
2026-08-16 00:00:36 +0530

    Fix variable name from closestsum to currentsum

--- closestsum.java
+++ closestsum.java
@@ -5,14 +5,13 @@ public class closestsum {
     int countTriplets(int sum, int[] arr) {
         Arrays.sort(arr);
         int count = 0;
-        sum =2;
         int i =0;
         for(i=0;i<arr.length-2;i++){
             int left = i+1;
             int right = arr.length-1;
             while(left<right){
-                 int closestsum = arr[i]+arr[left]+arr[right];
-                if(closestsum<sum){
+                 int currentsum = arr[i]+arr[left]+arr[right];
+                if(currentsum<sum){
                    count+=(right-left);
                    left++;
                 }else{
